9b4a8a359efe4ee2b539408cec7257e597e5f862
liuye11 <1840038994@qq.ocm>
2024-10-02 12:14:05 +0800

    简单的角色移动

---
+++ Assets/Scripts/Main/Main.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 这里面放全局游戏数据或者方法
+/// </summary>
+public class Main : MonoBehaviour
+{
+    public static int MaxHealth;
+}
---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl: MonoBehaviour
+{
+    public float speed;
+    private long currentHealth;
+    Rigidbody2D rb;
+    //public Animator animator;
+    private void Start()
+    {
+        currentHealth = Main.MaxHealth;
+        rb = GetComponent<Rigidbody2D>();
+/*        StartCoroutine(Shoot());
+        StartCoroutine(Dying());*/
+    }
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"), 0f);
+        //animator.SetFloat("Horizontal",movement.x);
+        rb.velocity = new Vector2(movement.x*speed, movement.y*speed);
+    }
+    IEnumerator Dying()
+    {
+        while (currentHealth > 0)
+        {
+            yield return 0;
+        }
+        Debug.Log("you are dead");
+    }
+    public void ChangeHealth(int value)
+    {
+        currentHealth += value;
+        //Debug.Log("oh hurt£¡");
+    }
+}
