3b2cfd93396c1cef110ad436761e4a416e1b05a4
liuye11 <1840038994@qq.com>
2024-10-06 10:08:04 +0800

    修复音乐bug

--- Assets/Scripts/Main/MusicControl.cs
+++ Assets/Scripts/Main/MusicControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InGameMusicController : MonoBehaviour
+public class MusicControl : MonoBehaviour
 {
     public AudioSource inGame;
     public AudioSource win;
